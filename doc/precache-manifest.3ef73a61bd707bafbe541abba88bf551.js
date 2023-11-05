bd12166b65537181d299729de955151b9303652c
maji <qkwl2001@163.com>
2023-11-05 12:33:17 +0800

    add doc

---
+++ doc/precache-manifest.3ef73a61bd707bafbe541abba88bf551.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aeb79d2bd674705c50959020aabc2a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "699f3e054b921bcd7432",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d4ca37cc057bc6f176f3",
+    "url": "/static/js/2.22743211.chunk.js"
+  },
+  {
+    "revision": "699f3e054b921bcd7432",
+    "url": "/static/js/main.494f16dc.chunk.js"
+  },
+  {
+    "revision": "36b3a9e4ca75c79f0d0f",
+    "url": "/static/js/runtime-main.b6607611.js"
+  }
+]);
